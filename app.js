5cfba9ee347628bb3d667633b31479e2d9516d15
Ola <ohagenbendixen@gmail.com>
2022-10-25 15:37:59 +0200

    Get the two password fields separately

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const passwordFieldsNodelist = document.querySelectorAll(".error")
 const passwordFields = Array.from(passwordFieldsNodelist);
 
 passwordFields.forEach(ele => ele.setCustomValidity("Invalid password"));
+
+
+const passwordField1 = document.querySelector("#password")
+const passwordField2 = document.querySelector("#confirm-password")
